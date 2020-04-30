3cd4ce7c6936c8e84e423453eaadeaa53d246d23
Vadim Zhukov <zhukov.vd.s@yandex.ru>
2020-04-30 19:47:54 +0300

    Lab_4

---
+++ Lab4/Lab4/main.cpp
@@ -0,0 +1,13 @@
+#include "task1.h"
+#include "task2.h"
+#include "task3.h"
+
+int main()
+{
+	task1_run();
+    	task2_run();
+    	task3_run();
+
+	system("pause");
+	return 0;
+}
---
+++ Lab4/Lab4/task3.h
@@ -0,0 +1,182 @@
+#include <vector>
+#include <thread>
+#include <iostream>
+#include <stdio.h>
+#include <exception>
+#include <locale.h>
+#include <cilk/cilk.h>
+#include <cilk/reducer_opadd.h>
+/// ������������, ������������ ��� ����� ����������� ����������
+/// ������� �������� �������: �� ������� ��� �� ��������
+enum class eprocess_type_3
+{
+	by_rows = 0,
+	by_cols
+};
+
+void InitMatrix_3( double** matrix, const size_t numb_rows, const size_t numb_cols )
+{
+	for ( size_t i = 0; i < numb_rows; ++i )
+	{
+		for ( size_t j = 0; j < numb_cols; ++j )
+		{
+			matrix[i][j] = rand() % 5 + 1;
+		}
+	}
+}
+
+/// ������� PrintMatrix() �������� �������� ������� <i>matrix</i> �� �������;
+/// numb_rows - ���������� ����� � �������� ������� <i>matrix</i>
+/// numb_cols - ���������� �������� � �������� ������� <i>matrix</i>
+void PrintMatrix_3( double** matrix, const size_t numb_rows, const size_t numb_cols )
+{
+	printf( "Generated matrix:\n" );
+	for ( size_t i = 0; i < numb_rows; ++i )
+	{
+		for ( size_t j = 0; j < numb_cols; ++j )
+		{
+			printf( "%lf ", matrix[i][j] );
+		}
+		printf( "\n" );
+	}
+}
+
+/// ������� FindAverageValues() ������� ������� �������� � ������� <i>matrix</i>
+/// �� �������, ���� �� �������� � ����������� �� �������� ��������� <i>proc_type</i>;
+/// proc_type - �������, � ����������� �� �������� ������� �������� ����������� 
+/// ���� �� �������, ���� �� ������� �������� ������� <i>matrix</i>
+/// matrix - �������� �������
+/// numb_rows - ���������� ����� � �������� ������� <i>matrix</i>
+/// numb_cols - ���������� �������� � �������� ������� <i>matrix</i>
+/// average_vals - ������, ���� ����������� ����������� ������� ��������
+void FindAverageValues_3( eprocess_type_3 proc_type, double** matrix, const size_t numb_rows, const size_t numb_cols, double* average_vals )
+{
+	switch ( proc_type )
+	{
+		case eprocess_type_3::by_rows:
+		{
+			for ( size_t i = 0; i < numb_rows; ++i )
+			{
+				// Меняем тип переменной sum, чтобы предотвратить гонку данных 
+				cilk::reducer_opadd <double> sum;
+				cilk_for( size_t j = 0; j < numb_cols; ++j )
+				{
+					sum += matrix[i][j];
+				}
+				average_vals[i] = sum.get_value() / numb_cols;
+			}
+			break;
+		}
+		case eprocess_type_3::by_cols:
+		{
+			for ( size_t j = 0; j < numb_cols; ++j )
+			{
+				// Меняем тип переменной sum, чтобы предотвратить гонку данных 
+				cilk::reducer_opadd <double> sum;
+				cilk_for( size_t i = 0; i < numb_rows; ++i )
+				{
+					sum += matrix[i][j];
+				}
+				average_vals[j] = sum.get_value() / numb_rows;
+			}
+			break;
+		}
+		default:
+		{
+			throw("Incorrect value for parameter 'proc_type' in function FindAverageValues() call!");
+		}
+	}
+}
+
+/// ������� PrintAverageVals() �������� �������� ������� <i>average_vals</i> �� �������;
+/// proc_type - �������, ���������� �� ��, ��� ���� ��������� 
+/// ������� �������� �������� ������� �� ������� ��� �� ��������
+/// average_vals - ������, �������� ������� �������� �������� �������,
+/// ����������� �� ������� ��� �� ��������
+/// dimension - ���������� ��������� � �������� ������� <i>average_vals</i>
+void PrintAverageVals_3( eprocess_type_3 proc_type, double* average_vals, const size_t dimension )
+{
+	switch ( proc_type )
+	{
+		case eprocess_type_3::by_rows:
+		{
+			printf( "\nAverage values in rows:\n" );
+			for ( size_t i = 0; i < dimension; ++i )
+			{
+				printf( "Row %u: %lf\n", i, average_vals[i] );
+			}
+			break;
+		}
+		case eprocess_type_3::by_cols:
+		{
+			printf( "\nAverage values in columns:\n" );
+			for ( size_t i = 0; i < dimension; ++i )
+			{
+				printf( "Column %u: %lf\n", i, average_vals[i] );
+			}
+			break;
+		}
+		default:
+		{
+			throw("Incorrect value for parameter 'proc_type' in function PrintAverageVals() call!");
+		}
+	}
+}
+
+
+void task3_run()
+{
+	const unsigned ERROR_STATUS = -1;
+	const unsigned OK_STATUS = 0;
+
+	unsigned status = OK_STATUS;
+
+	try
+	{
+		srand( (unsigned) time( 0 ) );
+
+		const size_t numb_rows = 2;
+		const size_t numb_cols = 3;
+
+		double** matrix = new double*[numb_rows];
+		for ( size_t i = 0; i < numb_rows; ++i )
+		{
+			matrix[i] = new double[numb_cols];
+		}
+
+		double* average_vals_in_rows = new double[numb_rows];
+		double* average_vals_in_cols = new double[numb_cols];
+
+		InitMatrix_3( matrix, numb_rows, numb_cols );
+		std::cout << "-------------------------------------------------------------------------\nBase program + cilk_for + memory clearing\n\n";
+
+		PrintMatrix_3( matrix, numb_rows, numb_cols );
+
+		auto start = clock() / 1000.0;
+		std::thread first_thr( FindAverageValues_3, eprocess_type_3::by_rows, matrix, numb_rows, numb_cols, average_vals_in_rows );
+		std::thread second_thr( FindAverageValues_3, eprocess_type_3::by_cols, matrix, numb_rows, numb_cols, average_vals_in_cols );
+	
+
+		first_thr.join();
+		second_thr.join();
+		auto end = clock() / 1000.0;
+		PrintAverageVals_3( eprocess_type_3::by_rows, average_vals_in_rows, numb_rows );
+		PrintAverageVals_3( eprocess_type_3::by_cols, average_vals_in_cols, numb_cols );
+		std::cout << "Duration: " << end-start << "\n";
+		
+		// Чистим память, чтобы предотвратить утечку 
+		delete[] average_vals_in_rows;
+		delete[] average_vals_in_cols;
+		for (size_t i = 0; i < numb_rows; ++i)
+				delete[] matrix[i];
+		delete[] matrix;
+	}
+	catch ( std::exception& except )
+	{
+		printf( "Error occured!\n" );
+		except.what();
+		status = ERROR_STATUS;
+	}
+
+	//return status;
+}
